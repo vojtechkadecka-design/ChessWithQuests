59da08030d82452b51cf39efdcd2910e0212cb9e
vojta <vojtech.kadecka@gjkt.eu>
2026-06-16 20:31:33 +0200

    Herní plocha dodělána.

--- model/herni_plocha.py
+++ model/herni_plocha.py
@@ -1,5 +1,20 @@
+from model.figurka import Figurka
+from model.tah import Tah
+
+
 class HerniPlocha:
     def __init__(self):
         self.rozmery = [(8,8)]
-        self
+        self.herni_deska = [(())]
+        self.vyhozené_figurky_1 = []
+        self.vyhozene_figurky_1 = []
+        self.tah(Tah)
+        self.figurka(Figurka)
+
+    def posun_figurky(self):
+        pass
+
+    def nahrad_figurku(self):
+        pass
+
 
